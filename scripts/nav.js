c583a7281a030cbfe58c3e3e923b6a64ba6130a2
JMZ WD <jmz-webdesigns@hotmail.com>
2026-03-13 18:05:03 +0000

    Initial commit

---
+++ scripts/nav.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    const hamburger = document.getElementById('hamburger');
+  
+    // Toggle the menu visibility
+    menu.classList.toggle('show');
+  
+    // Toggle the hamburger icon to cross (or vice versa)
+    hamburger.classList.toggle('open');
+  }
+  
+  function toggleMenu2() {
+    const menu = document.getElementById('menu');
+    const hamburger2 = document.getElementById('hamburger2');
+  
+    // Toggle the menu visibility
+    menu.classList.toggle('show');
+  
+    // Toggle the hamburger icon to cross (or vice versa)
+    hamburger2.classList.toggle('open');
+  }
+  
